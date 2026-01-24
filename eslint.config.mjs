7668730808af3f2e17ac87fae5d55e149e436d7b
PaiPai-Fish829 <baigeifish@gmail.com>
2026-01-24 21:42:21 +0800

    前端基础

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,38 @@
-import antfu from '@antfu/eslint-config'
+import antfu from '@antfu/eslint-config';
 
 export default antfu({
   react: true,
-})
+  typescript: true,
+  format: true,
+  ignores: [
+    '*.md',
+    '**/lottie/*.json',
+    '**/templates/*',
+    '**/ui/*',
+  ],
+  rules: {
+    'style/no-tabs': [
+      'error',
+      {
+        allowIndentationTabs: true,
+      },
+    ],
+    'unused-imports/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+      },
+    ],
+    'style/semi': [
+      'error',
+      'always',
+    ],
+    'jsdoc/check-alignment': 'off',
+    'no-console': 'off',
+  },
+});
